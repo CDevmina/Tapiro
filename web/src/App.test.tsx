51753fe0ec1457559b46fd09ac141c658b10cde5
CDevmina <chamathdewmina25@gmail.com>
2025-03-05 14:04:39 +0530

    Remove current web

--- web/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Vite + React heading", () => {
-  render(<App />);
-  const heading = screen.getByText(/Vite \+ React/i);
-  expect(heading).toBeInTheDocument();
-});
