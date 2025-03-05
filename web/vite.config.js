33c5f9fd74fe8461fdf67cf4cb8fe408a0d7b9f2
CDevmina <chamathdewmina25@gmail.com>
2025-03-05 14:58:13 +0530

    Hot Reloading for Web

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,9 +6,13 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true,
+      interval: 100, // Check for changes every 100ms
     },
     host: true,
     strictPort: true,
     port: 5173,
+    hmr: {
+      clientPort: 3001, // This is important for Docker
+    },
   },
 });
