d7185f6ab6120e3635c6decd8562e35e05d68820
CDevmina <chamathdewmina25@gmail.com>
2025-04-13 16:13:26 +0530

    New Flowbite Frontend setup.

--- web/src/api/index.js
+++
@@ -1,5 +0,0 @@
-export { useAuthApi } from "./authApi";
-export { useUserApi } from "./userApi";
-export { useStoreApi } from "./storeApi";
-export { usePreferencesApi } from "./preferencesApi";
-export { healthApi } from "./healthApi";
