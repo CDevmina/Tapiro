189af4e2cb1c61961d203fdf101114482c4a581c
CDevmina <chamathdewmina25@gmail.com>
2025-04-27 04:41:35 +0530

    Revert "feat: Add UserProfilePage route for user profile management"
    This reverts commit 46421dd89ba7159c98f23af366e987753b36441c.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -15,7 +15,6 @@ import { AuthProviderWrapper } from "./context/AuthContext";
 import PrivateRoute from "./components/auth/PrivateRoute";
 import NotFoundPage from "./pages/static/NotFoundPage";
 import StoreProfilePage from "./pages/StoreDashboard/StoreProfilePage";
-import UserProfilePage from "./pages/UserDashboard/UserProfilePage";
 
 const router = createBrowserRouter([
   {
@@ -45,10 +44,6 @@ const router = createBrowserRouter([
           {
             path: "dashboard/user",
             element: <UserDashboard />,
-          },
-          {
-            path: "profile/user",
-            element: <UserProfilePage />,
           },
         ],
       },
