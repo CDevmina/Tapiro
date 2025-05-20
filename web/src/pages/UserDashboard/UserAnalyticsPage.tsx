21d973fbf586bf640b110361a378789db5aa3b0a
CDevmina <chamathdewmina25@gmail.com>
2025-05-20 20:54:07 +0530

    feat: Update delete confirmation modal icon color for better visibility

--- web/src/pages/UserDashboard/UserAnalyticsPage.tsx
+++ web/src/pages/UserDashboard/UserAnalyticsPage.tsx
@@ -678,7 +678,7 @@ const UserAnalyticsPage: React.FC = () => {
         <ModalHeader />
         <ModalBody>
           <div className="text-center">
-            <HiExclamation className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <HiExclamation className="mx-auto mb-4 h-14 w-14 text-red-600 dark:text-red-600" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
               Are you sure you want to delete this data?
               {deletionScope === "today" &&
